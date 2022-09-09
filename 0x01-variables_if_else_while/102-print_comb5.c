03e08bc1677e3b5ed6fb35066e302dddeb3f004c
chrisdanielwise <chrisdanielwise@gmail.com>
2022-09-09 22:52:39 +0000

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints numbers between 00 to 99.
- *    
- * Return: Always 0 (Success)
+ *  main - Prints numbers between 00 to 99.
+ *     
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
