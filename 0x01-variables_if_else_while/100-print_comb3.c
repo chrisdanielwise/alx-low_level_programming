3084a486e6cfc00998de2d5f20d07b35f354f8ff
chrisdanielwise <chrisdanielwise@gmail.com>
2022-09-09 16:04:25 +0000

    my best commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  *main - Entry point, print 00 to 99 using putchar
@@ -8,16 +7,16 @@ int main(void)
 {
 int x;
 int y;
-for (x = 0; x <= 9; x++)
+for (x = '0' ; x <= '9' ; x++)
 {
-for (y = 0 ; y <= 9 ; y++)
+for (y = '0' ; y <= '9' ; y++)
 {
 putchar(x);
 putchar(y);
 if (!(x == '9' && y == '9'))
 {
 putchar(',');
-putchar(',');
+putchar(' ');
 }
 }
 }
