d9c26609bfb26f83087663e414405e45466ba3e4
chrisdanielwise <chrisdanielwise@gmail.com>
2022-09-13 15:01:41 +0000

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,6 @@ int main(void)
         _putchar('_');
         _putchar('p');
         _putchar('u');
-        _putchar('u');
         _putchar('t');
         _putchar('c');
         _putchar('h');
